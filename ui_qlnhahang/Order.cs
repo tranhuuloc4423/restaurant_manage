eee7c07a940ca80eb7e361b5ca70947ea59b1c0d
MahiruShN <minkuan1704@gmail.com>
2023-10-04 20:56:12 +0700

    fix order list class

--- ui_qlnhahang/Order.cs
+++ ui_qlnhahang/Order.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,66 +19,7 @@ namespace ui_qlnhahang
             InitializeComponent();
         }
 
-        public class OrderItem
-        {
-            public int FoodId { get; set; }
-            public string FoodName { get; set; }
-            public int Quantity { get; set; }
-            public decimal Price { get; set; }
 
-        public class OrderList
-        {
-            public int OrderId { get; set; }
-            public DateTime OrderDate { get; set; }
-            public List<OrderItem> OrderItems { get; set; }
-
-            public OrderList()
-            {
-                OrderItems = new List<OrderItem>();
-            }
-
-            public void AddItem(int foodId, string foodName, int quantity, decimal price)
-            {
-                OrderItem item = new OrderItem
-                {
-                    FoodId = foodId,
-                    FoodName = foodName,
-                    Quantity = quantity,
-                    Price = price
-                };
-
-                OrderItems.Add(item);
-                }
-
-            public void RemoveItem(int foodId)
-            {
-                OrderItem item = OrderItems.Find(i => i.FoodId == foodId);
-                if (item != null)
-                {
-                    OrderItems.Remove(item);
-                }
-            }
-
-            public void UpdateQuantity(int foodId, int newQuantity)
-            {
-                OrderItem item = OrderItems.Find(i => i.FoodId == foodId);
-                if (item != null)
-                {
-                    item.Quantity = newQuantity;
-                }
-            }
-
-            public int GetTotalQuantity()
-            {
-                int totalQuantity = 0;
-                foreach (OrderItem item in OrderItems)
-                {
-                    totalQuantity += item.Quantity;
-                }
-
-                return totalQuantity;
-            }
-        }
 
         void loadFoodlist()
         {
@@ -120,6 +60,66 @@ namespace ui_qlnhahang
         private void bunifuDropdown1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+    }
+
+    public class OrderItem
+    {
+        public int FoodId { get; set; }
+        public string FoodName { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class OrderListed
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+
+        public OrderListed()
+        {
+            OrderItems = new List<OrderItem>();
+        }
+
+        public void AddItem(int foodId, string foodName, int quantity)
+        {
+            OrderItem item = new OrderItem
+            {
+                FoodId = foodId,
+                FoodName = foodName,
+                Quantity = quantity
+            };
+
+            OrderItems.Add(item);
+        }
+
+        public void RemoveItem(int foodId)
+        {
+            OrderItem item = OrderItems.Find(i => i.FoodId == foodId);
+            if (item != null)
+            {
+                OrderItems.Remove(item);
+            }
+        }
+
+        public void UpdateQuantity(int foodId, int newQuantity)
+        {
+            OrderItem item = OrderItems.Find(i => i.FoodId == foodId);
+            if (item != null)
+            {
+                item.Quantity = newQuantity;
+            }
+        }
+
+        public int GetTotalQuantity()
+        {
+            int totalQuantity = 0;
+            foreach (OrderItem item in OrderItems)
+            {
+                totalQuantity += item.Quantity;
+            }
+
+            return totalQuantity;
         }
     }
 }
